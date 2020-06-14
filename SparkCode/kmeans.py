cb14442942cea111c99b220c84991bf077c43ae1
Francesco Ronchieri <francesco.ronchieri@imagicle.com>
2020-06-14 19:57:33 +0200

    Update kmeans.py

--- SparkCode/kmeans.py
+++ SparkCode/kmeans.py
@@ -1,27 +1,41 @@
 import sys
+import numpy
+import random
 from operator import add
 
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 7:
         print("Usage: KMeans <input file (points)> <dimensions> <centroids> <Stop Criteria> <max iteration> [<output file>]", file=sys.stderr)
         sys.exit(-1)
     inputFilePath = sys.argv[1]
-    inputFilePath = sys.argv[1]
+    pointsDimension = sys.argv[2]
+    numberOfCentroids = sys.argv[3]
+    stopCriteria = sys.argv[4]
+    maxIterations = sys.argv[5]
+    outputPath = sys.argv[6]
 
         # Print degli argomenti da riga di comando
-    print("INFO | args[0]: <input points> = ", file=sys.stdout)   #File di input
-    print("INFO | args[1]: <d> = ", file=sys.stdout)              # Numero componenti per punto
-    print("INFO | args[2]: <k> = ", file=sys.stdout)              # Numero centroidi = numero cluster
-    print("INFO | args[3]: Stop Criteria = ", file=sys.stdout)             
-    print("INFO | args[4]: <max_iterations> = ", file=sys.stdout)             
-    print("INFO | args[5]: <output file (centroids)> = ", file=sys.stdout)   # Cartella di output
+    print("INFO | args[0]: <input points> = {inputFilePath}")           #File di input
+    print("INFO | args[1]: <d> = {pointsDimension}")                    # Numero componenti per punto
+    print("INFO | args[2]: <k> = {numberOfCentroids}")                  # Numero centroidi => numero cluster
+    print("INFO | args[3]: Stop Criteria = {stopCriteria}")             
+    print("INFO | args[4]: <max_iterations> = {maxIterations}")             
+    print("INFO | args[5]: <output file (centroids)> = {outputPath}")   # Cartella di output
 
     master = "yarn"
     sc = SparkContext(master, "KMeans")
+    
 
-    #lines = sc.textFile(sys.argv[1])
+    lines = sc.textFile(inputFilePath)
+    points = lines.map(lambda x: x.split(",").astype(numpy.float))
+    withReplacement = False                                             #Il campo withReplacement = False, specifica che non sono ammessi duplicati
+    randomSeedValue = random.randrange(sys.maxsize)
+    print("INFO | seed to select centroids = {randomSeedValue}")
+    centroids = points.takeSample(withReplacement, numberOfCentroids, randomSeedValue)
+    sc.broadcast(centroids)                                             #Condivido in READ-ONLY i centroid per tutti i task spark
+    
     #words = lines.flatMap(lambda x: x.split(' '))
     #ones =  words.map(lambda x: (x, 1))
     #counts = ones.reduceByKey(add)

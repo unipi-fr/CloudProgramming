1ce48df59395b0de22f97252eda3f3f6a83d9397
Francesco Ronchieri <francesco.ronchieri@imagicle.com>
2020-06-29 00:00:57 +0200

    auto-deploy terminato

--- Vallati/deployEnviroment.py
+++ Vallati/deployEnviroment.py
@@ -23,7 +23,8 @@ def executeSSHCommand(sshSession,command):
     print(ssh_stdout.read().decode('ascii').strip("\n"))
     print(ssh_stderr.read().decode('ascii').strip("\n"))
 
-def configureBackEnd(machineIP,sshUser,sshPassword,exchange):
+def configureMachine(machineType,machineIP,sshUser,sshPassword,exchange):
+    print('\n[INFO] ---------- Configuring {machineType}[{machineIP}]'.format(machineType=machineType,machineIP=machineIP))
     configForBackEnd = {}
     configForBackEnd["zookeper-ip"] = config["zookeper-ip"]
     configForBackEnd["exchange"] = exchange
@@ -31,126 +32,89 @@ def configureBackEnd(machineIP,sshUser,sshPassword,exchange):
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(machineIP,22,username=sshUser,password=sshPassword,timeout=4)
 
-    executeSSHCommand(ssh,"rm -rf Back-End")
+    executeSSHCommand(ssh,'rm -rf {machineType}'.format(machineType=machineType))
 
     sftp = ssh.open_sftp()
-    sftp.put('Back-End.tar.gz', 'Back-End.tar.gz')
+    sftp.put('{machineType}.tar.gz'.format(machineType=machineType), '{machineType}.tar.gz'.format(machineType=machineType))
     
     
-    executeSSHCommand(ssh,"tar -xzvf Back-End.tar.gz")
-    executeSSHCommand(ssh,"rm -f Back-End.tar.gz")
-    with open('config-back-end.json', 'w') as json_file:
+    executeSSHCommand(ssh,'tar -xzvf {machineType}.tar.gz'.format(machineType=machineType))
+    executeSSHCommand(ssh,'rm -f {machineType}.tar.gz'.format(machineType=machineType))
+    with open('config-{machineType}.json'.format(machineType=machineType), 'w') as json_file:
         json.dump(configForBackEnd, json_file)
-    sftp.put('config-back-end.json', 'Back-End/back-end-server/config.json')
+    sftp.put('config-{machineType}.json'.format(machineType=machineType), '{machineType}/{machineType}-server/config.json'.format(machineType=machineType))
     sftp.close()
-    os.remove('config-back-end.json')
+    os.remove('config-{machineType}.json'.format(machineType=machineType))
+    
+    print('[INFO] cleaning configuration: container[{container}], images[{images}], base-images[{baseImages}]'
+    .format(
+        container=config['clean-{machineType}-container'.format(machineType=machineType)], 
+        images=config['clean-{machineType}-image'.format(machineType=machineType)],
+        baseImages=config['clean-base-{machineType}-image'.format(machineType=machineType)]))
+        
+    if config['clean-{machineType}-container'.format(machineType=machineType)] & checkRemoteContainerExists(ssh,'{machineType}-server'.format(machineType=machineType)):
+        print('[INFO] cleaning container...')
+        removeRemoteContainer(ssh,'{machineType}-server'.format(machineType=machineType))
+    if config['clean-{machineType}-image'.format(machineType=machineType)] & checkRemoteImageExists(ssh,'{machineType}-server'.format(machineType=machineType)):
+        print('[INFO] cleaning images...')
+        removeRemoteImage(ssh,'{machineType}-server'.format(machineType=machineType))
+    if config['clean-base-{machineType}-image'.format(machineType=machineType)] & checkRemoteImageExists(ssh,'{machineType}-base-server'.format(machineType=machineType)):
+        print('[INFO] cleaning base images...')
+        removeRemoteImage(ssh,'{machineType}-base-server'.format(machineType=machineType))
 
-    print("[INFO] cleaning configuration: container[%r], images[%r], baseImages[%r]" % (config["clean-back-end-container"], config["clean-back-end-image"], config["clean-base-back-end-image"]))
-    if config["clean-back-end-container"] & checkRemoteContainerExists(ssh,"back-end-server"):
-        print("[INFO] cleaning container...")
-        removeRemoteContainer(ssh,"back-end-server")
-    if config["clean-back-end-image"] & checkRemoteImageExists(ssh,"back-end-server"):
-        print("[INFO] cleaning images...")
-        removeRemoteImage(ssh,"back-end-server")
-    if config["clean-base-back-end-image"] & checkRemoteImageExists(ssh,"back-end-base-server"):
-        print("[INFO] cleaning base images...")
-        removeRemoteImage(ssh,"back-end-base-server")
-
-    if not checkRemoteImageExists(ssh,"back-end-base-server"):
-        print("[INFO] building back-end-base-server image")
-        buildRemoteImage(sshSession=ssh,remotePath="Back-End",nameImage="back-end-base-server",dockerfile="only-back-end-base-server.dockerfile")
+    if not checkRemoteImageExists(ssh,'{machineType}-base-server'.format(machineType=machineType)):
+        print('[INFO] building {machineType}-base-server image'.format(machineType=machineType))
+        buildRemoteImage(sshSession=ssh,
+                        remotePath='{machineType}'.format(machineType=machineType),
+                        nameImage='{machineType}-base-server'.format(machineType=machineType),
+                        dockerfile='only-{machineType}-base-server.dockerfile'.format(machineType=machineType))
     else:
-        print("[INFO] skipping building back-end-base-server image (alredy exits)")
-    if not checkRemoteImageExists(ssh,"back-end-server"):
-        print("[INFO] building back-end-server image")
-        buildRemoteImage(sshSession=ssh,remotePath="Back-End",nameImage="back-end-server",dockerfile="only-back-end-server.dockerfile")
+        print('[INFO] skipping building {machineType}-base-server image (alredy exits)'.format(machineType=machineType))
+    if not checkRemoteImageExists(ssh,'{machineType}-server'.format(machineType=machineType)):
+        print('[INFO] building {machineType}-server image')
+        buildRemoteImage(sshSession=ssh,
+                        remotePath='{machineType}'.format(machineType=machineType),
+                        nameImage='{machineType}-server'.format(machineType=machineType),
+                        dockerfile='only-{machineType}-server.dockerfile'.format(machineType=machineType))
     else:
-        print("[INFO] skipping building back-end-server image (alredy exits)")
-    if not checkRemoteContainerExists(ssh,"back-end-server"):
-        print("[INFO] depploing back-end-server")
-        executeSSHCommand(ssh,"docker run -d --hostname my-back-end --name back-end-server -p 8080:8080 back-end-server")
+        print('[INFO] skipping building {machineType}-server image (alredy exits)'.format(machineType=machineType))
+    if not checkRemoteContainerExists(ssh,'{machineType}-server'.format(machineType=machineType)):
+        print('[INFO] depploing {machineType}-server'.format(machineType=machineType))
+        executeSSHCommand(ssh,'docker run -d --hostname my-{machineType} --name {machineType}-server -p 8080:8080 {machineType}-server'.format(machineType=machineType))
     else:
-        print("[INFO] skipping deploy back-end-server container (alredy exits)")
+        print('[INFO] skipping deploy {machineType}-server container (alredy exits)'.format(machineType=machineType))
 
 def buildRemoteImage(sshSession,remotePath,nameImage,dockerfile):
-    executeSSHCommand(sshSession,"cd "+remotePath+"; docker build -t "+nameImage+" -f "+dockerfile+" .")
+    executeSSHCommand(sshSession,'cd {remotePath}; docker build -t {nameImage} -f {dockerfile} .'
+        .format(remotePath=remotePath,nameImage=nameImage,dockerfile=dockerfile))
 
 def removeRemoteContainer(sshSession,containerName):
-    executeSSHCommand(sshSession,"docker container rm -f "+containerName)
+    executeSSHCommand(sshSession,'docker container rm -f {containerName}'.format(containerName=containerName))
 
 def removeRemoteImage(sshSession,imageName):
-    executeSSHCommand(sshSession,"docker image rm -f "+imageName)
+    executeSSHCommand(sshSession,'docker image rm -f {imageName}'.format(imageName=imageName))
 
 def checkRemoteContainerExists(sshSession,containerName):
-    ssh_stdin, ssh_stdout, ssh_stderr = sshSession.exec_command('docker container ls | grep -w "'+containerName+'"')
+    ssh_stdin, ssh_stdout, ssh_stderr = sshSession.exec_command('docker container ls | grep -w "{containerName}"'.format(containerName=containerName))
     return len(ssh_stdout.readlines()) > 0
 
 def checkRemoteImageExists(sshSession,imageName):
-    ssh_stdin, ssh_stdout, ssh_stderr = sshSession.exec_command('docker image ls | grep -w "'+imageName+'"')
+    ssh_stdin, ssh_stdout, ssh_stderr = sshSession.exec_command('docker image ls | grep -w "{imageName}"'.format(imageName=imageName))
     return  len(ssh_stdout.readlines()) > 0
 
-def configureFrontEnd(machineIP,sshUser,sshPassword,exchange):
-    configForFrontEnd = {}
-    configForFrontEnd["zookeper-ip"] = config["zookeper-ip"]
-    configForFrontEnd["exchange"] = exchange
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(machineIP,22,username=sshUser,password=sshPassword,timeout=4)
-
-    executeSSHCommand(ssh,"rm -rf Front-End")
-
-    sftp = ssh.open_sftp()
-    sftp.put('Front-End.tar.gz', 'Front-End.tar.gz')
-    
-    
-    executeSSHCommand(ssh,"tar -xzvf Front-End.tar.gz")
-    executeSSHCommand(ssh,"rm -f Front-End.tar.gz")
-    with open('config-front-end.json', 'w') as json_file:
-        json.dump(configForFrontEnd, json_file)
-    sftp.put('config-front-end.json', 'Front-End/front-end-server/config.json')
-    sftp.close()
-    os.remove('config-front-end.json')
-
-    print("[INFO] cleaning configuration: container[%r], images[%r], baseImages[%r]" % (config["clean-front-end-container"], config["clean-front-end-image"], config["clean-base-front-end-image"]))
-    if config["clean-front-end-container"] & checkRemoteContainerExists(ssh,"front-end-server"):
-        print("[INFO] cleaning container...")
-        removeRemoteContainer(ssh,"front-end-server")
-    if config["clean-front-end-image"] & checkRemoteImageExists(ssh,"front-end-server"):
-        print("[INFO] cleaning images...")
-        removeRemoteImage(ssh,"front-end-server")
-    if config["clean-base-front-end-image"] & checkRemoteImageExists(ssh,"front-end-base-server"):
-        print("[INFO] cleaning base images...")
-        removeRemoteImage(ssh,"front-end-base-server")
-
-    if not checkRemoteImageExists(ssh,"front-end-base-server"):
-        print("[INFO] building front-end-base-server image")
-        buildRemoteImage(sshSession=ssh,remotePath="Front-End",nameImage="front-end-base-server",dockerfile="only-front-end-base-server.dockerfile")
-    else:
-        print("[INFO] skipping building front-end-base-server image (alredy exits)")
-    if not checkRemoteImageExists(ssh,"front-end-server"):
-        print("[INFO] building front-end-server image")
-        buildRemoteImage(sshSession=ssh,remotePath="Front-End",nameImage="front-end-server",dockerfile="only-front-end-server.dockerfile")
-    else:
-        print("[INFO] skipping building front-end-server image (alredy exits)")
-    if not checkRemoteContainerExists(ssh,"front-end-server"):
-        print("[INFO] depploing front-end-server")
-        executeSSHCommand(ssh,"docker run -d --hostname my-front-end --name front-end-server -p 8080:8080 front-end-server")
-    else:
-        print("[INFO] skipping deploy front-end-server container (alredy exits)")
-
 if __name__ == '__main__':
-    with open('config.json') as f:
+    with open('config.json','r') as f:
         config = json.load(f)
-        print("loading configuration:\n"+json.dumps(config))
-    tarDir("Front-End","Front-End")
+        print('loading configuration:\n{config}'.format(config=json.dumps(config, indent=4, sort_keys=True)))
+    tarDir('front-end','front-end')
         
-    for machine in config["front-end-machines"]:
-        configureFrontEnd(machine["ip"],machine["ssh-user"],machine["ssh-password"],machine["exchange"])
-    os.remove('Front-End.tar.gz')
+    for machine in config['front-end-machines']:
+        configureMachine('front-end',machine['ip'],machine['ssh-user'],machine['ssh-password'],machine['exchange'])
+    os.remove('front-end.tar.gz')
         
-    tarDir("Back-End","Back-End")
-    for machine in config["back-end-machines"]:
-       configureBackEnd(machine["ip"],machine["ssh-user"],machine["ssh-password"],machine["exchange"])
-    os.remove('Back-End.tar.gz')
+    tarDir('back-end','back-end')
+    for machine in config['back-end-machines']:
+       configureMachine('back-end',machine['ip'],machine['ssh-user'],machine['ssh-password'],machine['exchange'])
+    os.remove('back-end.tar.gz')
     
     
